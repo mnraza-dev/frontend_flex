93b024bc96b0ee6e4624c85fd658aa36549b7742
mnraza-dev <mnraza94@gmail.com>
2025-02-12 15:50:21 +0530

    updated README.md file

--- next.config.ts
+++ next.config.ts
@@ -1,11 +1,8 @@
 import type { NextConfig } from "next";
-
 const nextConfig: NextConfig = {
   reactStrictMode: true, 
   eslint: {
     ignoreDuringBuilds: true, 
   },
-
 };
-
 export default nextConfig;
